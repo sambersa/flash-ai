8e417ddf6b8a1c9f6477f4f8b37f8ab0a0b2bf4a
Sam Bersa <1sambersa@gmail.com>
2025-08-17 01:08:02 +0200

    feat: improve UI and functionality for AI interview platform
    - Enhanced /sign-in and /sign-up pages with proper error handling and corrected form inputs
    - Added a persistent navbar with website logo and name 'Flash AI'
    - Implemented homepage (/(root)/page.tsx) with 'Start an AI Interview' button
    - Improved overall look & feel for a more appealing and user-friendly interface
    - Added 'Your Interviews' section to track AI interviews
    - Added 'Take an Interview' section to display available interviews
    - Installed dayjs for date formatting
    - Created InterviewCard.tsx to display interview details effectively

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import { dummyInterviews } from '@/constants'
+import InterviewCard from '@/components/InterviewCard'
 
 const page = () => {
   return (
-    <div>Home Page</div>
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Master Your Job Interviews with Realistic AI Simulations!</h2>
+          <p className="text-sm">Simulate interviews anytime, anywhere, and track your progress over time.</p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image src="/robot.png" alt="robot-man" width={400} height={400} className="max-sm:hidden" />
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard key={interview.id} {...interview} />
+          ))}
+
+          {/*<p>You haven&apos;t taken any interviews yet</p> */}
+
+        </div>
+      </section>
+    </>
   )
 }
 
